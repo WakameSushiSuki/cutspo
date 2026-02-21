9d7303fc0e3d80d2cf3ca89a9f6e118a89beed7e
WakameSushiSuki <djwalt782@gmail.com>
2026-02-20 23:19:16 -0500

    rewrite soon :3
    removed the content of this repo because the rewrite will likely be in Rust with Yew---i never liked TypeScript and i really dont want it on my GitHub for everyone to see XD but im not privating this so it doesnt look like im ignoring the project

--- src/util.ts
+++
@@ -1,9 +0,0 @@
-export namespace Util {
-    export function typeName(obj: any) {
-        return obj?.constructor?.name ??
-            (obj === null ? 'null'
-                          : 'undefined');
-    }
-}
-
-export default Util;
